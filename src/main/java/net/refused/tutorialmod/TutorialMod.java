ec8e0607ed512fa5c059a3b8f35d80ef7062a3e8
TheRefused <excalibur1883@gmail.com>
2022-10-21 16:53:15 -0500

    pushing to github

---
+++ src/main/java/net/refused/tutorialmod/TutorialMod.java
@@ -0,0 +1,21 @@
+package net.refused.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+import net.refused.tutorialmod.item.ModItems;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		ModItems.registerModItems();
+
+	}
+}
